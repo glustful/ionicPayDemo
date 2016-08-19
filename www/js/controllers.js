d4fb8e1b81bf5445a867ecc0e824ed5325b9723f
glustful <852411097@qq.com>
2016-08-19 15:41:26 +0800

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,12 +7,12 @@ angular.module('starter.controllers', [])
     var myDate = new Date();
     var tradeNo = myDate.getTime();
     var alipayClass = navigator.alipay;
-    var rsa = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAMXAqtjyjgaYxlAu0gV8yRCzmpZ1/a5p+pS7QGk0UN8GtKehze3lz32ZiGiTzm1egnKSBhYnQVnORIIPVR6etyCOjH68B8bzI1z4qjrQSWun27xyzBrcx6E7MDScqSyTftxgLLxcqbeowom8VI9nBqjMpNbbK5Krg9XmdMVEUk8jAgMBAAECgYA1/+6nX1S5KG6+5oojRXgCP4kvHPkw72+lvZxRHiAXRtV+EQkStXZ7bbl6FSKBaxVkA8v1sLJ/u3upqFYuDlki5+aahz/9wDLk7QMJQ8SkCrwsaDn4STZ3JUICeZAbzN0iCpr9F1aaoN4ER44dbgGN7AKVEZNsEXXlRLowsK4GgQJBAONxJ+Lj+auEqJE8GoIEe6uyTosABqzKM0senJTCZFSJ1LRKmC1DYMYFlS0i1I9yHBsX8dJIVZJ5vjphtYPav6sCQQDelS6YrLa7iZMahxk6Rb3BVk06N2pQgixs57aI86MYuJ8xP0yMgA9eb/qLf3ogCOC6AvrykKW9WIBYp/vMYxZpAkEAvRnC2PFFD2cvo/k97OX/UQTiKs2tvxIqzYU5GLH7KxKh5/ixR8jieCICItH3uwBb/+bLp1N+2lcgY3gDrAo/GwJAJTyn8vrSB0XYylNXsRwBxtp9SQQEUTu12vJY40h/PJ+GYmLIl6jE6XOqYrQM5lcV4YimGvaAA9FOh4Zy5ZqeIQJBAMniFdq0DhQFOFqdpZGHbkpyBXY1vsqSU+VaHQVzXgJF83i5WRuEx97q7Knj+6UVGHLW++81G2+hWyHpe6YGjm4=";
+    var rsa = "";
     alipayClass.pay({
-      "partner":"2088121342639696",
+      "partner":"",
       "rsa_private":rsa,
       "rsa_public":rsa,
-      "seller":"yncysuper@163.com",
+      "seller":"",
       "subject":"测试文件",
       "body":"alipay demo",
       "price":"0.01",
