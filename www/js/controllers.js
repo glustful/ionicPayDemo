a7ab5df239cab78d7a35d49a4ae5d1292532f45c
guojunjun <852411097@qq.com>
2016-08-24 23:36:24 +0800

    merge

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,17 +7,18 @@ angular.module('starter.controllers', [])
     var myDate = new Date();
     var tradeNo = myDate.getTime();
     var alipayClass = navigator.alipay;
-    var rsa = "";
+    var rsa ="MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAN2G9VHLDQyeogKbaHSPf89a9u8symYSKM1ucaGlGRf0vUxO2SWn0YCFF0Z3c1cx0kISQRtzH2r1vb0FGsEwoQuEXq0FCEbbxQ1/E/ehTX0wPxrfADz5pVWvWzehQT7DS/IWbsSQOnUxMHKcN2Q3Uy3R02JrErSaJMj5gxCq1B/bAgMBAAECgYAQV9uTbDqLOszTCmobZ1bTkm9zV8ea3i4acdJ6TXbbeJfaoFqO16GyLJ95+GgO/8L2UuhwwP6JYpXb8xt1M0Q+NSK6b/QZLCEla+7qXoeEHJ2SAe56doK+QL34sI74inGHuYXFogT/vZN78f+f6wX6XQfS7OvTuicL66T7tabx0QJBAPEkexGhnxiuyt66kGp/LvuPe1rFtYv6zFt8qVL7lQ+YioU0AAHz9cfI4OvzoIG2Y0qy4p2d6T90dWzNh/PSvTMCQQDrLRY5KDlNULzgm/6sMi7ao7rdGfIQku85s5fBWDBuSH7BYqD8emYSjyVixc6wKhY98aSq14G0qHx+hju/tQK5AkEAiEQfL4VvTBlqFpbCiQu51vnrsQ3MW3+r/37giD4Wq6t9i2wpHPFpUC5Zu7v+VY5laY7QtFTJu8vpl0RFsSjDNQJAbYxTDczu5FT3pqkDuLKnoLEkGlo7p/zJ8tb1mdq6tKSQZwTKeNGLOpnzkE8UOHtiYPhu/CXnBYB2fXmJ24M2AQJALqxMOoK5m8ycrQr6onx3Q5TFsTvGoRX+SVtDzh2ON5J3MfL2FqnFx1bpNOtsMGKgP2/hgU1H47U9eywxXNHviA==";
+    var pubRsa="MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDDI6d306Q8fIfCOaTXyiUeJHkrIvYISRcc73s3vF1ZT7XN8RNPwJxo8pWaJMmvyTn9N4HQ632qJBVHf8sxHi/fEsraprwCtzvzQETrNRwVxLO5jVmRGi60j8Ue1efIlzPXV9je9mkjzOmdssymZkh2QhUrCmZYI/FCEa3/cNMW0QIDAQAB";
     alipayClass.pay({
-      "partner":"",
-      "rsa_private":rsa,
-      "rsa_public":rsa,
-      "seller":"",
-      "subject":"测试文件",
-      "body":"alipay demo",
-      "price":"0.01",
+      "partner":"2088422692929955",    //商户ID
+      "rsa_private":rsa,               //私钥
+      "rsa_public":pubRsa,                //公钥
+      "seller":"2088422692929955",    //收款支付宝账号或对应的支付宝唯一用户号
+      "subject":"共享停车",             //商品名称
+      "body":"共享停车支付宝支付",        //商品详情
+      "price":"0.01",                  //金额
       "tradeNo":tradeNo,
-      "timeout":"30m",
+      "timeout":"30m",                 //超时设置
       "notifyUrl":"http://www.baidu.com"
     },function(resultStatus){
       $ionicLoading.show({
@@ -35,7 +36,7 @@ angular.module('starter.controllers', [])
   };
   $scope.wxpay = function(){
     alert("wxpay demo");
-    
+
     var weixin = navigator.weixin;
     weixin.sendPayReq({
         "appid":"wxb4ba3c02aa476ea1",
@@ -43,13 +44,13 @@ angular.module('starter.controllers', [])
         "method":"get",
         "data":{}
     },function(retcode){
-        
+
         alert("success:"+retcode);
     },function(message){
         alert("sendPayReq:"+ message);
     });
   };
-  
+
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
